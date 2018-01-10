87d496c8beb108e027aaf504a5882e69ed21cb35
pjury <patrickajury@gmail.com>
2018-01-10 15:24:07 +0000

    modularity2!

--- blobs_game.py
+++ blobs_game.py
@@ -9,10 +9,47 @@ BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 STARTING_BLUE_BLOBS = 10
 STARTING_RED_BLOBS = 3
+STARTING_GREEN_BLOBS = 5
 
 game_display = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Blob World')
 clock = pygame.time.Clock()
+
+class BlueBlob(Blob):
+
+    def __init__(self, x_boundary, y_boundary):
+        Blob.__init__(self, (0, 0, 255), x_boundary, y_boundary)
+
+    def __add__(self, other_blob):
+        if other_blob.color == (255, 0, 0):
+            self.size  -= other_blob.size
+            other_blob.size -= self.size
+
+        elif other_blob.color == (0, 255, 0):
+            self.size += other_blob.size
+            other_blob.size = 0
+
+        elif other_blob.color == (0, 0, 255):
+            #For now nothing, maybe later we do something here
+            pass
+
+        else:
+            raise Exception('Tried to combine one or multiple blobs of unsupported color')
+
+#    def move_fast(self):
+#        self.x += random.randrange(-5,5)
+#        self.y += random.randrange(-5,5)
+
+class RedBlob(Blob):
+
+    def __init__(self, x_boundary, y_boundary):
+        Blob.__init__(self, (255, 0, 0), x_boundary, y_boundary)
+
+class GreenBlob(Blob):
+
+    def __init__(self, x_boundary, y_boundary):
+        Blob.__init__(self, (0, 255, 0), x_boundary, y_boundary)
+
 
 def draw_environment(blob_list):
     game_display.fill(WHITE)
@@ -20,32 +57,29 @@ def draw_environment(blob_list):
         for blob_id in blob_dict:
             blob = blob_dict[blob_id]
             pygame.draw.circle(game_display, blob.color, [blob.x, blob.y], blob.size)
-            blob.move_fast()
+            blob.move()
             blob.check_bounds()
     pygame.display.update()
 
 def main():
-    blue_blobs = dict(enumerate([BlueBlob(BLUE,WIDTH,HEIGHT) for i in range(STARTING_BLUE_BLOBS)]))
-    red_blobs = dict(enumerate([BlueBlob(RED,WIDTH,HEIGHT) for i in range(STARTING_RED_BLOBS)]))
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+    blue_blobs = dict(enumerate([BlueBlob(WIDTH,HEIGHT) for i in range(STARTING_BLUE_BLOBS)]))
+    red_blobs = dict(enumerate([RedBlob(WIDTH,HEIGHT) for i in range(STARTING_RED_BLOBS)]))
+    green_blobs = dict(enumerate([GreenBlob(WIDTH,HEIGHT) for i in range(STARTING_GREEN_BLOBS)]))
 
-        draw_environment([blue_blobs, red_blobs])
-        clock.tick(60)
+    print('Current blue size: {}. Current red size: {}'.format(str(blue_blobs[0].size),
+                                                               str(red_blobs[0].size)))
 
+    blue_blobs[0] + red_blobs[0]
+    print('Current blue size: {}. Current red size: {}'.format(str(blue_blobs[0].size),
+                                                               str(red_blobs[0].size)))
+    #while True:
+    #    for event in pygame.event.get():
+    #        if event.type == pygame.QUIT:
+    #            pygame.quit()
+    #            quit()
 
-class BlueBlob(Blob):
-
-    def __init__(self, color, x_boundary, y_boundary):
-        super().__init__(color, x_boundary, y_boundary)
-        self.color = BLUE
-
-    def move_fast(self):
-        self.x += random.randrange(-5,5)
-        self.y += random.randrange(-5,5)
+    #    draw_environment([blue_blobs, red_blobs, green_blobs])
+    #    clock.tick(60)
 
 
 if __name__ == '__main__':
